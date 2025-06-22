f15353f1771673849c65f74d43d24f9384080152
Liam Nutley <evildeadfan123@gmail.com>
2025-06-22 20:12:18 +0100

    Update testbookDao.py
    Updated formatting.

--- testbookDao.py
+++ testbookDao.py
@@ -1,12 +1,11 @@
 from BookDao import bookDao
 
-
 book = {
     'ISBN':1234567,
     'price': 12,
     'author': 'Stephen King',
     'title': 'The Shining'
-
 }
+
 returnvalue = bookDao.create(book)
 print(returnvalue)
